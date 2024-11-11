232d093e67aaa667646e5a5f8e8500c6bd9a2682
Daniel Wilder <dauncy@gmail.com>
2024-11-11 09:18:19 -1000

    minor style improvements

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-full size-full flex-1 items-center justify-center flex">
+    <div className="flex min-h-screen size-full flex-1 items-center justify-center flex bg-zinc-50">
       <Link 
         href={'/search'} 
-        className="cursor-pointer shadow rounded-md bg-slate-600 text-white font-medium px-1.5 py-2 flex"
+        className="cursor-pointer shadow rounded-md bg-slate-600 text-white font-semibold px-1.5 py-2 flex"
       >
         {"Search ->"}
       </Link>
